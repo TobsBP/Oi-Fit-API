5e4e4bf03f3384a8c3b664fb3863553cedfb4d6a
tobsBP <tobias.bp2004@gmail.com>
2026-01-21 23:29:49 -0300

    feat: added users route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify';
 import { authRoute } from './routes/auth';
+import { usersRoute } from './routes/users';
 
 export const routes = async (app: FastifyInstance) => {
-	app.register(authRoute);
+  app.register(authRoute);
+	app.register(usersRoute)
 };
