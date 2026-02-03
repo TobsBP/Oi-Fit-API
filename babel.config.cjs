5e3481511f79ed76ef2efe37b7c0fd6e74ee4f2e
tobsBP <tobias.bp2004@gmail.com>
2026-02-02 21:19:10 -0300

    fix lint

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,33 +1,33 @@
 const plugins = [
-  [
-    'module-resolver',
-    {
-      root: ['./src'],
-      alias: {
-        '@': './src',
-      },
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-  ],
-  ['babel-plugin-add-import-extension', { extension: 'js' }],
+	[
+		'module-resolver',
+		{
+			root: ['./src'],
+			alias: {
+				'@': './src',
+			},
+			extensions: ['.js', '.jsx', '.ts', '.tsx'],
+		},
+	],
+	['babel-plugin-add-import-extension', { extension: 'js' }],
 ];
 
 if (process.env.NODE_ENV === 'production') {
-  plugins.push('transform-remove-console');
+	plugins.push('transform-remove-console');
 }
 
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-        modules: false,
-      },
-    ],
-    '@babel/preset-typescript',
-  ],
-  plugins,
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 'current',
+				},
+				modules: false,
+			},
+		],
+		'@babel/preset-typescript',
+	],
+	plugins,
 };
